fe4e699beb2659cf94b794e48c077968fe87f55e
MaxLiu <3140101098@zju.edu.cn>
2018-05-30 18:59:03 +0800

    second

--- ShirleyTracer/ShirleyTracer/src/main.cpp
+++ ShirleyTracer/ShirleyTracer/src/main.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
 #include "Bitmap.h"
 #include <memory>
+//#include "vec3.h"
+#include "ray.h"
+
+float hit_sphere(const vec3& center, float radius,const ray& r) {
+	vec3 oc = r.origin() - center;
+	float a = dot(r.direction(), r.direction());
+	float b = 2 * dot(oc, r.direction());
+	float c = dot(oc, oc) - radius * radius;
+	float discriminant = b * b - 4* a * c;
+//	return (discriminant > 0);
+	if (discriminant < 0)
+		return -1;
+	else {
+		return (-b - sqrt(discriminant)) / (2.0 * a);
+	}
+
+}
+
+vec3 color(const ray& r) {
+	float t = hit_sphere(vec3(0, 0, -1), 0.5, r);
+	if (t > 0.0) {
+		vec3 N = unit_vector(r.point_at_parameter(t) - vec3(0, 0, -1));
+		return 0.5*vec3(N.x() + 1, N.y() + 1, N.z() + 1);
+	}
+
+	vec3 unit_direction = unit_vector(r.direction());
+	 t = 0.5*(unit_direction.y() + 1.0);
+	return (1.0 - t)*vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+
+}
 
 int main() {
 
@@ -8,17 +38,25 @@ int main() {
 	int ny = 100;
 	std::auto_ptr<Bitmap> pic(new Bitmap(nx, ny));
 
+	vec3 lower_left_corner(-2.0, -1.0, -1.0);
+	vec3 horizontal(4.0, 0.0, 0.0);
+	vec3 vertical(0.0, 2.0, 0.0);
+	vec3 origin(0.0, 0.0,0.0);
 	for(int j = ny-1;j>=0;j--)
 		for (int i = 0; i < nx; i++) {
-			float r = float(i) / float(nx);
-			float g = float(j) / float(ny);
-			float b = 0.2;
-			int ir = int(255.99*r);
-			int ig = int(255.99*g);
-			int ib = int(255.99*b);
+			float u = float(i) / float(nx);
+			float v = float(j) / float(ny);
+			ray r(origin, lower_left_corner + u * horizontal + v * vertical);
+			vec3 col = color(r);
+			//			float b = 0.2;
+
+
+			int ir = int(255.99*col[0]);
+			int ig = int(255.99*col[1]);
+			int ib = int(255.99*col[2]);
 			pic->SetPixel(i,j, (ir << 16) | (ig << 8) | ib);
 		}
 
-	pic->SaveBMP("1.bmp");
+	pic->SaveBMP("4.bmp");
 
 }
---
+++ ShirleyTracer/ShirleyTracer/src/sphere.h
@@ -0,0 +1,43 @@
+#pragma once
+#include "hitable.h"
+
+class sphere : public hitable {
+public:
+	sphere() {};
+	sphere(vec3 cen, float r) : center(cen), radius(r) {};
+	virtual bool hit(const ray& r, float tmin, float tmax, hit_record& rec) const;
+
+	float get_radius() const{ return radius; }
+	vec3 get_center() const{ return center; }
+
+private:
+	vec3 center;
+	float radius;
+};
+
+bool sphere::hit(const ray& r, float t_min, float t_max, hit_record& rec) const {
+	vec3 oc = r.origin() - center;
+	float a = dot(r.direction(), r.direction());
+	float b = dot(oc, r.direction());
+	float c = dot(oc, oc) - radius * radius;
+	float discriminant = b * b - a * c;
+	if (discriminant > 0) {
+		float temp = (-b - sqrt(discriminant)) / a;
+		if (temp < t_max && temp > t_min) {
+			rec.t = temp;
+			rec.p = r.point_at_parameter(rec.t);
+			rec.normal = (rec.p - center) / radius;
+			rec.mat_ptr = mat_ptr;
+			return true;
+		}
+		temp = (-b + sqrt(discriminant)) / a;
+		if (temp < t_max && temp > t_min) {
+			rec.t = temp;
+			rec.p = r.point_at_parameter(rec.t);
+			rec.normal = (rec.p - center) / radius;
+			rec.mat_ptr = mat_ptr;
+			return true;
+		}
+	}
+	return false;
+}
